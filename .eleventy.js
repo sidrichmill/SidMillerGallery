003ec2bd18f6600167343bf0ee50bb02d331605c
sidrichmill <51D7H3K1D@gmail.com>
2021-09-02 10:31:53 -0700

    Delete old code

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,9 @@
-const del = require('del');
-var justifiedLayout = require('justified-layout')
-
 module.exports = function(eleventyConfig) {
 
     eleventyConfig.addPassthroughCopy('./src/style');
     eleventyConfig.addPassthroughCopy('./src/assets');
     eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addPassthroughCopy('./src/js');
-
-   // eleventyConfig.addShortcode("justifiedLayout", justifiedLayout(input, config)); // {{ justifiedLayout input, config }}
 
 
     eleventyConfig.addCollection("detailsCollection", function (collection) {
